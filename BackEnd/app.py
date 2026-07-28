125f9b65d1008f05e55b5ac0482d2a43787e8197
Farenas9134 <farenas9134@gmail.com>
2026-07-27 17:38:09 -0700

    added Flask blueprints for modularity

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from blueprints.helloworld.helloworld import helloworld_bp
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World! Food!!!</h1>"
+app.register_blueprint(helloworld_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
