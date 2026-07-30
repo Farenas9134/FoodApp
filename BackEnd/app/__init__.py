df63084c37c9c47ace335e4640a8f2f525f2c274
shalimmontes <shalimmontesh@gmail.com>
2026-07-29 21:18:52 -0500

    Started working on creating database with SQLAlchemy and SQLMigrate

--- BackEnd/app/__init__.py
+++ BackEnd/app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
 from .routes.helloworld import helloworld_bp
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
 
     # Register blueprint
     app.register_blueprint(helloworld_bp)
